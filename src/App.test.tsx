a172febbb81dfb9c51f7e0b3a62c5e1e59062f5e
Abhishek Patel <patelabhishek@coffeebeans.io>
2024-09-03 12:01:24 +0530

    Initial commit - removing all files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import App from './App';
-
-test('renders a compound button with correct data test id', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('compound-button');
-  expect(linkElement).toBeInTheDocument();
-});
